00e4f30ab8e5d79bf95bdc53872120ea06d148d7
Franklin <tchfrnkln@gmail.com>
2022-11-18 11:55:04 +0100

    comment out output.css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 // import './assets/input.css'
-import '../dist/output.css'
+// import '../dist/output.css'
 
 const app = createApp(App)
 
